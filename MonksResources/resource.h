f1c80906c1696d3b885a6d34afcbe095a5e3c22b
DESKTOP-HE222DN\piao <xuyun018@gmail.com>
2026-04-04 01:30:28 +0800

    no message

--- MonksResources/resource.h
+++ MonksResources/resource.h
@@ -4,7 +4,7 @@
 
 #define IDI_MAIN_ICON			101
 
-#define IDBS_COUNT													69
+#define IDBS_COUNT													120
 
 // shield judge
 
@@ -29,57 +29,57 @@
 
 // shield judge win
 
-#define IDB_BEARER_QGENERICBEARER_DLL1													120
-#define IDB_ICONENGINES_QSVGICON_DLL1													121
-#define IDB_IMAGEFORMATS_QGIF_DLL1													122
-#define IDB_IMAGEFORMATS_QICNS_DLL1													123
-#define IDB_IMAGEFORMATS_QICO_DLL1													124
-#define IDB_IMAGEFORMATS_QJPEG_DLL1													125
-#define IDB_IMAGEFORMATS_QSVG_DLL1													126
-#define IDB_IMAGEFORMATS_QTGA_DLL1													127
-#define IDB_IMAGEFORMATS_QTIFF_DLL1													128
-#define IDB_IMAGEFORMATS_QWBMP_DLL1													129
-#define IDB_IMAGEFORMATS_QWEBP_DLL1													130
-#define IDB_LIBGCC_S_DW2_1_DLL1													131
-#define IDB_LIBSTDC___6_DLL1													132
-#define IDB_LIBWINPTHREAD_1_DLL1													133
-#define IDB_MANIFEST_TXT1													134
-#define IDB_OFFICER_EXE1													135
-#define IDB_PLATFORMS_QWINDOWS_DLL1													136
-#define IDB_QT5CORE_DLL1													137
-#define IDB_QT5GUI_DLL1													138
-#define IDB_QT5NETWORK_DLL1													139
-#define IDB_QT5SQL_DLL1													140
-#define IDB_QT5SVG_DLL1													141
-#define IDB_QT5WIDGETS_DLL													142
-#define IDB_SQLDRIVERS_QSQLITE_DLL1													143
-#define IDB_SQLDRIVERS_QSQLMYSQL_DLL1													144
-#define IDB_SQLDRIVERS_QSQLODBC_DLL1													145
-#define IDB_SQLDRIVERS_QSQLPSQL_DLL1													146
-#define IDB_STYLES_QWINDOWSVISTASTYLE_DLL1													147
-#define IDB_TRANSLATIONS_QT_AR_QM1													148
-#define IDB_TRANSLATIONS_QT_BG_QM1													149
-#define IDB_TRANSLATIONS_QT_CA_QM1													150
-#define IDB_TRANSLATIONS_QT_CS_QM1													151
-#define IDB_TRANSLATIONS_QT_DA_QM1													152
-#define IDB_TRANSLATIONS_QT_DE_QM1													153
-#define IDB_TRANSLATIONS_QT_EN_QM1													154
-#define IDB_TRANSLATIONS_QT_ES_QM1													155
-#define IDB_TRANSLATIONS_QT_FI_QM1													156
-#define IDB_TRANSLATIONS_QT_FR_QM1													157
-#define IDB_TRANSLATIONS_QT_GD_QM1													158
-#define IDB_TRANSLATIONS_QT_HE_QM1													159
-#define IDB_TRANSLATIONS_QT_HU_QM1													160
-#define IDB_TRANSLATIONS_QT_IT_QM1													161
-#define IDB_TRANSLATIONS_QT_JA_QM1													162
-#define IDB_TRANSLATIONS_QT_KO_QM1													163
-#define IDB_TRANSLATIONS_QT_LV_QM1													164
-#define IDB_TRANSLATIONS_QT_PL_QM1													165
-#define IDB_TRANSLATIONS_QT_RU_QM1													166
-#define IDB_TRANSLATIONS_QT_SK_QM1													167
-#define IDB_TRANSLATIONS_QT_UK_QM1													168
-#define IDB_TRANSLATIONS_QT_ZH_TW_QM1													169
-#define IDB_UNINST_EXE1													170
+#define IDB_BEARER_QGENERICBEARER_DLL													120
+#define IDB_ICONENGINES_QSVGICON_DLL													121
+#define IDB_IMAGEFORMATS_QGIF_DLL													122
+#define IDB_IMAGEFORMATS_QICNS_DLL													123
+#define IDB_IMAGEFORMATS_QICO_DLL													124
+#define IDB_IMAGEFORMATS_QJPEG_DLL													125
+#define IDB_IMAGEFORMATS_QSVG_DLL													126
+#define IDB_IMAGEFORMATS_QTGA_DLL													127
+#define IDB_IMAGEFORMATS_QTIFF_DLL													128
+#define IDB_IMAGEFORMATS_QWBMP_DLL													129
+#define IDB_IMAGEFORMATS_QWEBP_DLL													130
+#define IDB_LIBGCC_S_DW2_1_DLL													131
+#define IDB_LIBSTDC___6_DLL													132
+#define IDB_LIBWINPTHREAD_1_DLL													133
+#define IDB_MANIFEST_TXT2													134
+#define IDB_OFFICER_EXE													135
+#define IDB_PLATFORMS_QWINDOWS_DLL													136
+#define IDB_QT5CORE_DLL													137
+#define IDB_QT5GUI_DLL													138
+#define IDB_QT5NETWORK_DLL													139
+#define IDB_QT5SQL_DLL													140
+#define IDB_QT5SVG_DLL													141
+#define IDB_QT5WIDGETS_DLL1													142
+#define IDB_SQLDRIVERS_QSQLITE_DLL													143
+#define IDB_SQLDRIVERS_QSQLMYSQL_DLL													144
+#define IDB_SQLDRIVERS_QSQLODBC_DLL													145
+#define IDB_SQLDRIVERS_QSQLPSQL_DLL													146
+#define IDB_STYLES_QWINDOWSVISTASTYLE_DLL													147
+#define IDB_TRANSLATIONS_QT_AR_QM													148
+#define IDB_TRANSLATIONS_QT_BG_QM													149
+#define IDB_TRANSLATIONS_QT_CA_QM													150
+#define IDB_TRANSLATIONS_QT_CS_QM													151
+#define IDB_TRANSLATIONS_QT_DA_QM													152
+#define IDB_TRANSLATIONS_QT_DE_QM													153
+#define IDB_TRANSLATIONS_QT_EN_QM													154
+#define IDB_TRANSLATIONS_QT_ES_QM													155
+#define IDB_TRANSLATIONS_QT_FI_QM													156
+#define IDB_TRANSLATIONS_QT_FR_QM													157
+#define IDB_TRANSLATIONS_QT_GD_QM													158
+#define IDB_TRANSLATIONS_QT_HE_QM													159
+#define IDB_TRANSLATIONS_QT_HU_QM													160
+#define IDB_TRANSLATIONS_QT_IT_QM													161
+#define IDB_TRANSLATIONS_QT_JA_QM													162
+#define IDB_TRANSLATIONS_QT_KO_QM													163
+#define IDB_TRANSLATIONS_QT_LV_QM													164
+#define IDB_TRANSLATIONS_QT_PL_QM													165
+#define IDB_TRANSLATIONS_QT_RU_QM													166
+#define IDB_TRANSLATIONS_QT_SK_QM													167
+#define IDB_TRANSLATIONS_QT_UK_QM													168
+#define IDB_TRANSLATIONS_QT_ZH_TW_QM													169
+#define IDB_UNINST_EXE													170
 
 
 #endif
